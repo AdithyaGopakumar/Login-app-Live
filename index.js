f130f656e37abb52b0b8b9c5fe0525418218d6a9
AdithyaGopakumar <adithyagopa@gmail.com>
2023-01-24 10:29:00 +0530

    env added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const db = require("./db.js");
-// const PORT = process.env.PORT;
-const PORT = process.env.PORT || 5000;
+require("dotenv").config();
+const port = process.env.PORT;
 let cors = require("cors");
 app.use(cors());
 
 const AuthController = require("./controller/AuthContoller");
 app.use("/api/auth", AuthController);
 
-app.listen(PORT, () => {
-  console.log(`listening to port ${PORT}`);
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
 });
