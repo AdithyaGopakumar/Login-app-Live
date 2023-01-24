c0a051d38656e217826b6dcaee744fdc94f715d0
AdithyaGopakumar <adithyagopa@gmail.com>
2023-01-24 18:59:32 +0530

    without env

--- db.js
+++ db.js
@@ -1,4 +1,7 @@
 let mongoose = require("mongoose");
 require("dotenv").config();
-mongoose.connect(`${process.env.MongoURL}`);
+// mongoose.connect(`${process.env.MongoURL}`);
+mongoose.connect(
+  "mongodb+srv://test:test123@cluster0.prgajkd.mongodb.net/?retryWrites=true&w=majority"
+);
 // mongoose.connect("mongodb://localhost:27017/healthkart");
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 const db = require("./db.js");
-require("dotenv").config();
+// require("dotenv").config();
 let cors = require("cors");
 app.use(cors());
 
-const PORT = process.env.PORT;
+const port = 5000;
 
 const AuthController = require("./controller/AuthContoller");
 app.use("/api/auth", AuthController);
 
-// app.listen(port, () => {
-//   console.log(`listening to port ${port}`);
-// });
-app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
-);
+app.listen(port, () => {
+  console.log(`listening to port ${port}`);
+});
+// app.listen(process.env.PORT, () =>
+//   console.log(`Server running on port ${process.env.PORT}`)
+// );
