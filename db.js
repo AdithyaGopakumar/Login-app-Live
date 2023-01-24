ea06d6675728563d0b0405bc1b95c36d18b38aca
AdithyaGopakumar <adithyagopa@gmail.com>
2023-01-24 09:18:30 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,5 @@
+let mongoose = require("mongoose");
+mongoose.connect(
+  "mongodb+srv://test:test123@cluster0.prgajkd.mongodb.net/?retryWrites=true&w=majority"
+);
+// mongoose.connect("mongodb://localhost:27017/healthkart");
