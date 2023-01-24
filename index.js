199341c45db53d2c5985916e3106a1ddea4bf94a
AdithyaGopakumar <adithyagopa@gmail.com>
2023-01-24 10:13:36 +0530

    new port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const db = require("./db.js");
-const PORT = 5000;
+// const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 let cors = require("cors");
 app.use(cors());
 
