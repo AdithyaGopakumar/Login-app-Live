46b385a15ed3ad00401e79adb40a5067ba35d1be
AdithyaGopakumar <adithyagopa@gmail.com>
2023-01-24 11:19:56 +0530

    mongo url added in env

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 let mongoose = require("mongoose");
-mongoose.connect(
-  "mongodb+srv://test:test123@cluster0.prgajkd.mongodb.net/?retryWrites=true&w=majority"
-);
+require("dotenv").config();
+mongoose.connect(`${process.env.MongoURL}`);
 // mongoose.connect("mongodb://localhost:27017/healthkart");
